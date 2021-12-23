da59c964eae8fccd0d47ead44c3918de695014cd
Ahmed <atomcgarry@gmail.com>
2021-12-23 14:12:39 +0000

    set up

---
+++ src/app.ts
@@ -0,0 +1,25 @@
+import express, {Request, Response} from "express";
+
+const app = express();
+app.use(express.json()) // Allows us to pass JSO N bodys in post request.
+
+app.get("/", (req: Request, res: Response) => {
+  return res.json({
+      success: true,
+      name: "Ahmed is a baws"
+  })
+});
+
+app.post("/api/data", (req: Request, res: Response) => {
+  console.log(req.body);
+  return res.sendStatus(200);
+});
+
+// All basically means that we can make all types of req, like get post put etc
+app.all('/api/all', (req: Request, res: Response) => {
+    return res.sendStatus(200);
+})
+
+app.listen(3000, () => {
+  console.log("application listening at http://localhost:3000");
+});
