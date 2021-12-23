8fcec895c5cfebd3ebddc6b8297d4d50c8fffbad
Ahmed <atomcgarry@gmail.com>
2021-12-23 15:03:41 +0000

    connection file is up

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -1,1 +1,14 @@
 import { connect } from "mongoose";
+
+const connection = async (): Promise<void>  => {
+    try {
+        await connect("mongodb+srv://ahmedmcgarry:test123@babajohns.xspjn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+        console.log("Connected")
+        return
+    } catch (error) {
+        console.log(error)
+        return
+    }
+}
+
+connection();
