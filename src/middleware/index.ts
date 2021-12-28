fb34587bc38fc207dc49c8a9c77865d6892a7d22
Ahmed <atomcgarry@gmail.com>
2021-12-28 13:59:43 +0000

    compare passwords function

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,5 +1,17 @@
-import User from '../user/user.model'
+import User from "../user/user.model";
+import { Request, Response } from "express";
 
-export const tokenAuth = () => {}
-export const hashPassword = () => {}
-export const comparePasswords = () => {}
+export const tokenAuth = () => {};
+export const hashPassword = () => {};
+export const comparePasswords = async (req: Request, res: Response, next: Function): Promise<void> => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    if (req.body.pass === user?.password) {
+      req.user = user;
+      next();
+    }
+  } catch (error) {
+      console.log(error)
+      res.sendStatus(401).send({message: 'incorrect login details'})
+  }
+};
